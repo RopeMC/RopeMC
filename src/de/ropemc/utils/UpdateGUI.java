7d91529946c2c28ef7804416abbaf9a1c0ebdf42
Jake <jakeesser@me.com>
2017-09-26 17:46:59 +0200

    :3

--- src/de/ropemc/utils/UpdateGUI.java
+++ src/de/ropemc/utils/UpdateGUI.java
@@ -1,13 +1,11 @@
 package de.ropemc.utils;
 
 import javax.swing.*;
-import java.awt.*;
 
 public class UpdateGUI {
     public static JFrame frame;
 
     public static void createGUI() {
-        UpdateGUI here = new UpdateGUI();
         frame = new JFrame("Updating RopeMC...");
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -19,16 +17,7 @@ public class UpdateGUI {
         JProgressBar pb = new JProgressBar();
         pb.setIndeterminate(true);
         frame.add(pb);
-        here.centerFrame();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-    }
-    private void centerFrame() {
-        Dimension windowSize = frame.getSize();
-        GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
-        Point centerPoint = ge.getCenterPoint();
-
-        int dx = centerPoint.x - windowSize.width / 2;
-        int dy = centerPoint.y - windowSize.height / 2;
-        frame.setLocation(dx, dy);
     }
 }
