904f5417f56cf49aa52c7a605e4ef2b437a2626e
Bloupix <BlueWolf165@gmx.de>
2017-11-19 11:43:47 +0100

    nvm

--- src/de/ropemc/Transformer.java
+++ src/de/ropemc/Transformer.java
@@ -49,7 +49,7 @@ public class Transformer implements ClassFileTransformer {
             try {
                 ClassPool cp = ClassPool.getDefault();
                 CtClass cc = cp.get(Mappings.getClassName("net.minecraft.client.gui.GuiIngame"));
-                CtMethod m = cc.getDeclaredMethod(Mappings.getMethodName("net.minecraft.client.gui.GuiIngame", "renderGameOverlay"));
+                CtMethod m = cc.getDeclaredMethod(Mappings.getMethodName("net.minecraft.client.gui.GuiIngame", "renderGameOverlay"), new CtClass[]{CtClass.floatType});
                 m.insertAt(121, "de.ropemc.Hooks.draw2DHook();");
                 byte[] byteCode = cc.toBytecode();
                 cc.detach();
