0ce43a3ca797e27b880456c1795b4b552c31d160
HGGamers_ <HGGamers@t-online.de>
2018-05-07 13:58:29 +0200

    UpdaterGUI: Changed location from images to resources folder

--- src/main/java/de/ropemc/updater/UpdaterGUI.java
+++ src/main/java/de/ropemc/updater/UpdaterGUI.java
@@ -11,18 +11,20 @@ import javafx.stage.Screen;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
+import java.util.Objects;
+
 public class UpdaterGUI extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("UpdaterGUI.fxml"));
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getClassLoader().getResource("updater/UpdaterGUI.fxml")));
         primaryStage.setTitle("Updating RopeMC...");
         primaryStage.setScene(new Scene(root, primaryStage.getWidth(), primaryStage.getHeight()));
         primaryStage.getScene().setFill(Color.TRANSPARENT);
         primaryStage.initStyle(StageStyle.TRANSPARENT);
         primaryStage.setResizable(false);
         primaryStage.setAlwaysOnTop(true);
-        primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("RopeMC.png")));
+        primaryStage.getIcons().add(new Image(Objects.requireNonNull(getClass().getClassLoader().getResourceAsStream("updater/RopeMC.png"))));
         primaryStage.show();
 
         Rectangle2D screenBounds = Screen.getPrimary().getVisualBounds();
